cb337e47f31f1fa9974edc567723d0c789ec178e
ibtissam zennibi <ibtissamzennibii@gmail.com>
2023-09-05 16:57:36 -0700

    ok

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,60 +1,26 @@
 #include "lists.h"
 
 /**
- * check_cycle - f#include "lists.h"
- *
- */**
  * check_cycle - function checking if singly linked list has a cycle or not
- *  * @list: pointer the beginning #include "lists.h"
- *
- */**
-  * check_cycle - function checking if singly linked list has a cycle or not
-  *  * @list: pointer the beginning of #include "lists.h"
-  *
-  */**
-   * check_cycle - function checking if singly linked list has a cycle or not
-   *  * @list: pointer the beginning of the#include "lists.h"
-   *
-   */**
-    * check_cycle - function checking if singly linked list has a cycle or not
-    *  * @list: pointer the beginning of the no#include "lists.h"
-    *
-    */**
-     * check_cycle - function checking if singly linked list has a cycle or not
-     *  * @list: pointer the beginning of the node
-#include "lists.h"
-
-     /**
-      *  * check_cycle - function checking if singly linked list has a cycle or not
-      *   * @list: pointer the beginning of the node
-    #include "lists.h"
-
-    /**
-     *  * check_cycle - function checking if singly linked list has a cycle or not
-     *   * @list: pointer the beginning of the node
-  #include "lists.h"
-
-  /**
-   *  * check_cycle - function checking if singly linked list has a cycle or not
-   *   * @list: pointer the beginning of the node
-   *    * Return: 0 for no cycle, 1 for a cycle
-   *     */
-     int check_cycle(listint_t *list)
+ * @list: pointer the beginning of the node
+ * Return: 0 for no cycle, 1 for a cycle
+ */
+int check_cycle(listint_t *list)
 {
-		listint_t *current, *check;
+	listint_t *current, *check;
 
-			if (list == NULL || list->next == NULL)
-						return (0);
-				current = list;
-					check = current->next;
+	if (list == NULL || list->next == NULL)
+		return (0);
+	current = list;
+	check = current->next;
 
-						while (current != NULL && check->next != NULL
-										&& check->next->next != NULL)
-						{
-									if (current == check)
-													return (1);
-											current = current->next;
-													check = check->next->next;
-						}
-							return (0);
+	while (current != NULL && check->next != NULL
+		&& check->next->next != NULL)
+	{
+		if (current == check)
+			return (1);
+		current = current->next;
+		check = check->next->next;
+	}
+	return (0);
 }
